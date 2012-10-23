af24757fb9e974a2e191861233cf0d136babc91c
Antonio <rssntn67@yahoo.it>
2012-10-23 09:47:52 +0200

    Changed main method for Rest client api

--- opennms-rest-client/src/main/java/org/opennms/rest/client/internal/JerseyClientImpl.java
+++ opennms-rest-client/src/main/java/org/opennms/rest/client/internal/JerseyClientImpl.java
@@ -5,8 +5,11 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 
 import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.GenericType;
 import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.json.JSONConfiguration;
 import com.sun.jersey.client.apache.ApacheHttpClient;
+import com.sun.jersey.client.apache.config.ApacheHttpClientConfig;
 import com.sun.jersey.client.apache.config.DefaultApacheHttpClientConfig;
 
 public class JerseyClientImpl {
@@ -18,16 +21,17 @@ public class JerseyClientImpl {
     WebResource m_webResource;
     
     public JerseyClientImpl(String url, String username, String password) {
-        
+    	
         m_config = new DefaultApacheHttpClientConfig();
+        m_config.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING,Boolean.TRUE);
+        m_config.getProperties().put(ApacheHttpClientConfig.PROPERTY_PREEMPTIVE_AUTHENTICATION, Boolean.TRUE);
         m_config.getState().setCredentials(null, null,-1, username, password);
         m_client = ApacheHttpClient.create(m_config);
         m_webResource = m_client.resource(url);
-        
     }
     
     public <T> T get(Class<T> clazz,String relativePath, MultivaluedMap<String, String> queryParams) {
-        return m_webResource.path(relativePath).queryParams(queryParams).accept(MediaType.APPLICATION_XML_TYPE).get(clazz);
+        return m_webResource.path(relativePath).queryParams(queryParams).header("Accept", "application/xml").accept(MediaType.APPLICATION_XML_TYPE).get(new GenericType<T>(clazz));
     }
 
     public <T> T get(Class<T> clazz,String relativePath) {
